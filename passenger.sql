519a47c5c2282aa8b6120c680d64dd4b94a35741
na0260 <samjr.008@gmail.com>
2023-06-05 19:58:25 +0600

    updated SQL file added

--- passenger.sql
+++ passenger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 04, 2023 at 01:30 PM
+-- Generation Time: Jun 05, 2023 at 01:56 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -73,6 +73,68 @@ INSERT INTO `agent__requests` (`id`, `name`, `mail`, `organization`, `phone`, `c
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `buses`
+--
+
+CREATE TABLE `buses` (
+  `id` bigint UNSIGNED NOT NULL,
+  `organization` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `number` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_stoppage` int NOT NULL,
+  `start_point` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `end_point` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `start_time` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `end_time` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `buses`
+--
+
+INSERT INTO `buses` (`id`, `organization`, `name`, `number`, `total_stoppage`, `start_point`, `end_point`, `start_time`, `end_time`, `created_at`, `updated_at`) VALUES
+(1, 'New', 'Bus-1', 'D-XXXX', 10, 'XXX', 'YYY', '06:00', '23:00', '2023-06-05 11:25:58', '2023-06-05 11:25:58'),
+(2, 'test', 'Bus-1', 'DM - 123122', 5, 'X', 'Y', '06:00', '23:00', '2023-06-05 11:33:51', '2023-06-05 11:33:51'),
+(3, 'X transport', 'XBus - 1', 'DM - ka XX-XXXX', 10, 'X', 'Y', '06:00', '23:00', '2023-06-05 12:19:48', '2023-06-05 12:19:48'),
+(4, 'X transport', 'XBus - 2', 'DM - ka XX-XXXY', 5, 'A', 'Z', '06:00', '13:00', '2023-06-05 12:39:08', '2023-06-05 12:39:08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bus_routes`
+--
+
+CREATE TABLE `bus_routes` (
+  `id` bigint UNSIGNED NOT NULL,
+  `organization` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bus_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `station_01` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `station_02` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `station_03` text COLLATE utf8mb4_unicode_ci,
+  `station_04` text COLLATE utf8mb4_unicode_ci,
+  `station_05` text COLLATE utf8mb4_unicode_ci,
+  `station_06` text COLLATE utf8mb4_unicode_ci,
+  `station_07` text COLLATE utf8mb4_unicode_ci,
+  `station_08` text COLLATE utf8mb4_unicode_ci,
+  `station_09` text COLLATE utf8mb4_unicode_ci,
+  `station_10` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `bus_routes`
+--
+
+INSERT INTO `bus_routes` (`id`, `organization`, `bus_name`, `station_01`, `station_02`, `station_03`, `station_04`, `station_05`, `station_06`, `station_07`, `station_08`, `station_09`, `station_10`, `created_at`, `updated_at`) VALUES
+(1, 'New', 'Bus-1', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', '2023-06-05 12:08:09', '2023-06-05 12:08:09'),
+(2, 'X transport', 'XBus - 1', '1', '2', '3', '4', '5', NULL, NULL, NULL, NULL, NULL, '2023-06-05 12:21:30', '2023-06-05 12:21:30');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `contacts`
 --
 
@@ -96,6 +158,33 @@ INSERT INTO `contacts` (`id`, `name`, `mail`, `phone`, `subject`, `desc`, `creat
 (3, 'my full name', 'me@mydomain.com', '12312312312', 'Subject', 'Write Description', '2023-06-04 11:27:36', '2023-06-04 11:27:36'),
 (5, 'my full name', 'me@mydomain.com', '12312312312', 'Subject', 'this is a comment', '2023-06-04 11:30:30', '2023-06-04 11:30:30'),
 (6, 'my full name', 'me@mydomain.com', '12312312312', 'Subject', 'this is a comment', '2023-06-04 11:30:44', '2023-06-04 11:30:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drivers`
+--
+
+CREATE TABLE `drivers` (
+  `id` bigint UNSIGNED NOT NULL,
+  `organization` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bus_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `number` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nid` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `driving_license` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` text COLLATE utf8mb4_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `drivers`
+--
+
+INSERT INTO `drivers` (`id`, `organization`, `bus_name`, `name`, `number`, `nid`, `driving_license`, `address`, `created_at`, `updated_at`) VALUES
+(1, 'New', 'Bus-1', 'Bus-1 Driver', '01010101011', '121212121212', 'DK-123456DS123', NULL, '2023-06-05 11:53:41', '2023-06-05 11:53:41'),
+(2, 'X transport', 'XBus - 1', 'XDriver', '01000000000', '1111111111', 'DK-123456789', 'XXXX', '2023-06-05 12:20:53', '2023-06-05 12:20:53');
 
 -- --------------------------------------------------------
 
@@ -140,7 +229,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (8, '2023_05_30_214706_create_agent__requests_table', 2),
 (10, '2023_05_31_163124_create_contacts_table', 3),
 (11, '2023_05_31_170459_alter_table', 4),
-(12, '2023_06_01_171251_create_views_table', 5);
+(12, '2023_06_01_171251_create_views_table', 5),
+(20, '2023_06_01_194038_create_buses_table', 6),
+(21, '2023_06_04_184425_create_drivers_table', 6),
+(22, '2023_06_04_184441_create_bus_routes_table', 6);
 
 -- --------------------------------------------------------
 
@@ -252,7 +344,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `phone`, `type`, `organization`, `em
 (3, 'new', 'agent@mail.com', NULL, 'admin', NULL, NULL, '$2y$10$9fO1Ln5zSAX0jX94yoJkWOlOLaGWsi3FwLBS/ZfUh7FCPNy7Y3H7q', 'active', NULL, '2023-06-03 19:31:04', '2023-06-03 19:31:04'),
 (4, 'new 2', 'agentaa@mail.com', '12312312312', 'agent', 'asdasdad', NULL, '$2y$10$v72gulp9BK0dF97goNQP4Oz11900UdA3tRyWPjgBQP70AeJOs/6LW', 'active', NULL, '2023-06-03 19:35:32', '2023-06-03 19:58:59'),
 (5, 'my full name', 'me@mydomain.com', '12312312312', 'agent', 'test', NULL, '$2y$10$LrfJlihXPszvBMGs3/7Jbu9bXCFzRsf9s9Nq4U8TPdD/7ENDViwSq', 'active', NULL, '2023-06-04 10:56:30', '2023-06-04 10:56:30'),
-(6, 'Agent', 'agentmail@mail.com', '01234567890', 'agent', 'New', NULL, '$2y$10$nJwiEU7h/4qWP2EHFYoiL.EeUaKt8fjf2iHEATFGQRAxKUopG3im2', 'active', NULL, '2023-06-04 11:19:21', '2023-06-04 11:19:21');
+(6, 'Agent', 'agentmail@mail.com', '01234567890', 'agent', 'New', NULL, '$2y$10$nJwiEU7h/4qWP2EHFYoiL.EeUaKt8fjf2iHEATFGQRAxKUopG3im2', 'active', NULL, '2023-06-04 11:19:21', '2023-06-04 11:19:21'),
+(7, 'AgentX', 'agentx@mail.com', '01700000000', 'agent', 'X transport', NULL, '$2y$10$BBV9eVY.WymXY5MXemvyQevEpE8kAWb8DXBHYvo2EOw5VNGG7Tl4C', 'active', NULL, '2023-06-05 12:10:52', '2023-06-05 12:10:52');
 
 --
 -- Indexes for dumped tables
@@ -271,9 +364,27 @@ ALTER TABLE `agent__requests`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `buses`
+--
+ALTER TABLE `buses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bus_routes`
+--
+ALTER TABLE `bus_routes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `contacts`
 --
 ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `drivers`
+--
+ALTER TABLE `drivers`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -339,10 +450,28 @@ ALTER TABLE `agent__requests`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `buses`
+--
+ALTER TABLE `buses`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `bus_routes`
+--
+ALTER TABLE `bus_routes`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `contacts`
 --
 ALTER TABLE `contacts`
   MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `drivers`
+--
+ALTER TABLE `drivers`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -354,7 +483,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -378,7 +507,7 @@ ALTER TABLE `terms`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
