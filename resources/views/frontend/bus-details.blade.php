1edd23b59c2d42f31d3b2a7ddbd7a8ac32a0fa8f
na0260 <samjr.008@gmail.com>
2023-06-14 12:38:44 +0600

    Bus details page created

---
+++ resources/views/frontend/bus-details.blade.php
@@ -0,0 +1,34 @@
+<x-app title="Privacy | Passenger">
+    <x-slot name="content">
+        <!--- banner-1 ---->
+        <div class="banner-1 ">
+            <div class="container">
+                <h1 class="wow zoomIn animated animated" data-wow-delay=".5s"
+                    style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> Passengers - Best in Country for Local Bus Information</h1>
+            </div>
+        </div>
+        <!--- /banner-1 ---->
+        <!--- privacy ---->
+        <div class="privacy">
+            <div class="container">
+                <h3 class="wow fadeInDown animated animated" data-wow-delay=".5s"
+                    style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Bus Details</h3>
+                <h4 class="wow fadeInDown animated animated" data-wow-delay=".5s"
+                    style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Bus Information</h4>
+                <p class="wow fadeInDown animated animated" data-wow-delay=".5s"
+                    style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Name: </p>
+                <p class="wow fadeInDown animated animated" data-wow-delay=".5s"
+                    style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Number: </p>
+                <p class="wow fadeInDown animated animated" data-wow-delay=".5s"
+                    style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Organization: </p>
+                <h4 class="wow fadeInDown animated animated" data-wow-delay=".5s"
+                    style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Driver Information</h4>
+                <p class="wow fadeInDown animated animated" data-wow-delay=".5s"
+                   style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Name:</p>
+                <p class="wow fadeInDown animated animated" data-wow-delay=".5s"
+                   style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">Contact Number:</p>
+            </div>
+        </div>
+        <!--- /privacy ---->
+    </x-slot>
+</x-app>
